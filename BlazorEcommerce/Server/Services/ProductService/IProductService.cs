fe6c9330cd8ba2c0348b9a523097a5fb42cd3ad2
Brandon Lackey <bslackey@gmail.com>
2022-11-02 15:08:26 -0700

    injection crap

--- BlazorEcommerce/Server/Services/ProductService/IProductService.cs
+++ BlazorEcommerce/Server/Services/ProductService/IProductService.cs
@@ -2,5 +2,6 @@
 {
     public interface IProductService
     {
+        Task<ServiceResponse<List<Product>>> GetProductsAsync();
     }
 }
--- BlazorEcommerce/Server/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Server/Services/ProductService/ProductService.cs
@@ -2,5 +2,20 @@
 {
     public class ProductService : IProductService
     {
+        private readonly DataContext _context;
+
+        public ProductService(DataContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<ServiceResponse<List<Product>>> GetProductsAsync()
+        {
+            var response = new ServiceResponse<List<Product>>
+            {
+                Data = await _context.Products.ToListAsync()
+            };
+            return response;    
+        }
     }
 }
